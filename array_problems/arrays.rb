8ccbf8fa735af721c6d849027e3f8a5ba38c396f
vagrant <vagrant@student-dev-box>
2014-06-06 11:00:17 -0500

    Add working first two tests

--- array_problems/arrays.rb
+++ array_problems/arrays.rb
@@ -1,8 +1,19 @@
 require 'rubygems'
-require 'pry-debugger'
+#require 'pry-debugger'
 
 module ArrayUtil
   def self.max(array)
+  	if array.length == 0
+  		return nil
+  	elsif array.length == 1
+  		return array[0]
+  	elsif array.length == 2
+  		if array[0] > array[1]
+  			return array[0]
+  		else
+  			return array[1]
+  		end
+  	end
   end
 
   def self.middle_element(array)
